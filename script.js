e526f92a272fd4d342974ae5589787c8f4e77d30
Josie Prescott <josieprescott88@gmail.com>
2025-11-11 09:56:38 -0700

    Add footer with copyright and last updated info

---
+++ script.js
@@ -0,0 +1,8 @@
+footer.innerHTML = `
+  <p>
+    &copy; ${new Date().getFullYear()} | Josie Prescott |
+    <a href="https://tooeletech.edu/">Tooele Technical College</a>
+    <br />
+    Last updated: ${currentDate}
+  </p>
+`;

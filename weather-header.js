40dbc17c720c5d7dc26b58a287e72025b450bde6
Josie Prescott <josieprescott88@gmail.com>
2025-11-13 08:46:05 -0700

    Remove redundant code and clean up formatting

--- weather-header.js
+++ weather-header.js
@@ -63,20 +63,7 @@ fetch(forecastURL)
         });
     });
 
-const footer = document.getElementById("footer");
-const currentDate = new Date().toLocaleDateString("en-US", {
-  year: "numeric",
-  month: "long",
-  day: "numeric"
-});
 
-footer.innerHTML = `
-  <p>
-    &copy; ${new Date().getFullYear()} | Josie Prescott |
-    <a href="https://tooeletech.edu/">Tooele Technical College</a>
-    <br />
-    Last updated: ${currentDate}
-  </p>
-`;
+
 
 
